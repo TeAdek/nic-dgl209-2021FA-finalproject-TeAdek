f805f0c0865c05ca2d9e82abaeb8b204e77c39e6
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-14 23:08:23 -0800

    Connect products to Product Screen properly

--- frontend/src/Product.js
+++ frontend/src/Product.js
@@ -1,9 +1,25 @@
 import React from "react";
+import data from "./data";
+import { Link, useParams } from 'react-router-dom';
 
 function Product(){
-   return(
+    const { id } = useParams(); // Unpacking and retrieve id
+    const product = data.products.find((x) => x.id === Number(id));
+    if(!product){
+        return <div>Product Not Found</div>;
+    }
+
+    return(
        <div>
-           <h1>Hi</h1>
+           <div className="row">
+               <img src={product.images} alt={product.productName}/>
+               <h1>{product.productName}</h1>
+               <div>Add to Cart</div>
+               <div>
+                   <h3>Description</h3>
+                   <p>{product.description}</p>
+               </div>
+               </div>
        </div>
    );
 }
