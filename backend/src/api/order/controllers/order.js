7fe82a6d8d878c66cca2dc1966af70e1fe295bf2
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-03-14 21:01:24 -0700

    Install and charge amount using Stripe
    install stripe inside the backend folder

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -3,11 +3,30 @@
 /**
  *  order controller
  */
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::order.order', ({strapi})=>({
     async create(ctx){
         const {amount,shippingAddress,city,state,pin,token} = ctx.require.body
+        await stripe.charges.create({
+            amount: amount * 100,
+            currency:"CAD",
+            source:token,
+            description:`order by user ${ctx.state.user.email}`
+          })
+
+        const order = await strapi.db.query('api::order.order').create({
+            data:{
+                shippingAddress,
+                city,
+                state,
+                amount,
+                pin,
+                user:ctx.state.user.email
+            }
+        })
+        return order
     }
 }));
